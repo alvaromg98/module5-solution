5f99e26bd3cc46fc4041d30c52aaa59d6031eabf
alvaro.mendoza-gonzalez@t-systems.com <alvaro.mendoza-gonzalez@t-systems.com>
2021-11-26 14:21:42 +0100

    log

--- js/script.js
+++ js/script.js
@@ -115,7 +115,7 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       
-      console.log(chosenCategoryShortName);
+      console.log(chosenCategoryShortName.url);
       var homeHtmlToInsertIntoMainPage = homeHtml;
       homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlToInsertIntoMainPage, "randomCategoryShortName", chosenCategoryShortName.url);
 
